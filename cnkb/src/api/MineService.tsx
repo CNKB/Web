dd96844e58b08a994f5204a36748db50f1d0a43a
namsic <52943870+namsic6460@users.noreply.github.com>
2021-12-09 21:39:21 +0900

    Finish mining

--- cnkb/src/api/MineService.tsx
+++ cnkb/src/api/MineService.tsx
@@ -3,8 +3,20 @@ import ServiceProp from "./ServiceProp";
 
 class MineService implements Service {
 	handleData(props: ServiceProp): void{
-		// let {status, data} = props.response
-		props.addMessage(JSON.stringify(props.response))
+		const { status, data } = props.response
+		
+		if(status === 200) {
+			const beforeMap = data["before"]
+			const minedMap = data["mined"]
+
+			let before, mined;
+			for (let key of Object.keys(minedMap)) {
+				before = beforeMap[key]
+				mined = minedMap[key]
+
+				props.addMessage(`${props.$(key)} ${mined}개를 캤습니다 (${before} -> ${before + mined})`)
+			}
+		}
 	}
 }
 
